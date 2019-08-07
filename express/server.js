afbdde229fd35649d03c29dec8c0b132a8938744
wpeterr <mycodeisbad@gmail.com>
2019-08-07 16:49:02 -0400

    asdf

---
+++ express/server.js
@@ -0,0 +1,21 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.send("GET request to the homepage");
+});
+router.get("/sites/:site/isLogged/:ip/true", function(req, res) {
+  res.send('true')
+});
+
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
